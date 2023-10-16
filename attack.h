08e4389165ed06e67c1fe0a1edc2d5fbe8fb10fa
Katelyn McCarthy Freeman <kmc313@cs23117fw.canterbury.ac.nz>
2023-10-16 19:28:36 +1300

    added enum for symbols

--- attack.h
+++ attack.h
@@ -3,6 +3,14 @@
 
 #include <stdbool.h>
 
+typedef enum  {
+    LIGHTNING,
+    SWORD,
+    BAMBOOZ,
+    NUKE,
+    NUM_SYMBOLS
+} Symbols;
+
 bool attack_check(void);
 void attack_switch(char attack);
 void attack_lightning(void);
@@ -10,5 +18,6 @@ void attack_sword(void);
 void attack_bambooz(void);
 void attack_nuke(void);
 void attack_choose(void);
+
 
 #endif
